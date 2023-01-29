d35fa7e4a794e2e88485ed50d1b13825c40ad3aa
kayton <kaytoncwc@gmail.com>
2023-01-30 03:16:05 +0800

    replcae image with img

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,22 +15,22 @@ export default function Home() {
         <div>
           {
             Array.from(Array(400).keys()).map((i) => {
-               return <div key={i} style={{height: 200}}>
-                 <div style={{display: 'inline-flex', height: 200, width: 200, position: 'relative'}}> 
-                <Image fill style={{objectFit: 'contain'}} src={`https://raw.githubusercontent.com/Aegide/custom-fusion-sprites/main/CustomBattlers/1.${i}.png`} alt={`original-1-${i}`} />
+              return <div key={i} style={{ height: 200 }}>
+                <div style={{ display: 'inline-flex', height: 200, width: 200, position: 'relative' }}>
+                  <img style={{ objectFit: 'contain' }} src={`https://raw.githubusercontent.com/Aegide/custom-fusion-sprites/main/CustomBattlers/1.${i}.png`} alt={`original-1-${i}`} />
 
-                 </div>
-                 <div style={{display: 'inline-flex', height: 200, width: 200, position: 'relative'}}> 
-               <Image fill src={`https://raw.githubusercontent.com/Aegide/autogen-fusion-sprites/master/Battlers/1/1.${i}.png`} alt={`custom-1-${i}`} />
                 </div>
-                <div style={{display: 'inline-flex', height: 200, width: 200, position: 'relative'}}> 
-               <Image fill src={`https://raw.githubusercontent.com/Aegide/custom-fusion-sprites/main/CustomBattlers/${i}.1.png`} alt={`original-${i}-1`} />
+                <div style={{ display: 'inline-flex', height: 200, width: 200, position: 'relative' }}>
+                  <img style={{ objectFit: 'contain' }} src={`https://raw.githubusercontent.com/Aegide/autogen-fusion-sprites/master/Battlers/1/1.${i}.png`} alt={`custom-1-${i}`} />
                 </div>
-                <div style={{display: 'inline-flex', height: 200, width: 200, position: 'relative'}}> 
-               <Image fill src={`https://raw.githubusercontent.com/Aegide/autogen-fusion-sprites/master/Battlers/${i}/${i}.1.png`} alt={`custom-${i}-1`} />
+                <div style={{ display: 'inline-flex', height: 200, width: 200, position: 'relative' }}>
+                  <img style={{ objectFit: 'contain' }} src={`https://raw.githubusercontent.com/Aegide/custom-fusion-sprites/main/CustomBattlers/${i}.1.png`} alt={`original-${i}-1`} />
+                </div>
+                <div style={{ display: 'inline-flex', height: 200, width: 200, position: 'relative' }}>
+                  <img style={{ objectFit: 'contain' }} src={`https://raw.githubusercontent.com/Aegide/autogen-fusion-sprites/master/Battlers/${i}/${i}.1.png`} alt={`custom-${i}-1`} />
                 </div>
 
-               </div>
+              </div>
             })
           }
           {/* {
